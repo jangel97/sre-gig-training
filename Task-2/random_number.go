d0972e4154112a7ed2e65c97335b3e95797c62f5
Jose Angel Morena <jmorenas@redhat.com>
2024-02-22 00:47:32 +0100

    adding exercise 2

--- Task-2/random_number.go
+++ Task-2/random_number.go
@@ -6,21 +6,23 @@ import (
 	"time"
 )
 
-// Function to generate a random number between 1 and 100
+// Function to generate a random number between 1 and 100 using a local random generator
 func generateRandomNumber() int {
-	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
-	return rand.Intn(100) + 1        // Return a random number between 1 and 100
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	return r.Intn(100) + 1
 }
 
-// Function to evaluate and print messages based on the generated number
+// Function to evaluate and print messages based on the generated number using a switch statement
 func evaluateAndPrintNumber(number int) {
-	if number > 50 && number%2 == 0 {
+	switch {
+	case number > 50 && number%2 == 0:
 		fmt.Println("It's closer to 100, and it's even!")
-	} else if number > 50 {
+	case number > 50:
 		fmt.Println("It's closer to 100")
-	} else if number == 50 {
+	case number == 50:
 		fmt.Println("It's 50!")
-	} else {
+	default:
 		fmt.Println("It's closer to 0")
 	}
 	fmt.Println("Generated number:", number)
